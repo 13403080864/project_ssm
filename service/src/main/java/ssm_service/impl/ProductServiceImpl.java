3f364fb7c4882391925113d6f54bb12f455fef24
13403080864 <480524962@qq.com>
2021-06-30 18:02:12 +0800

    COMMIT2

--- service/src/main/java/ssm_service/impl/ProductServiceImpl.java
+++ service/src/main/java/ssm_service/impl/ProductServiceImpl.java
@@ -2,6 +2,8 @@ package ssm_service.impl;
 
 import domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ssm_dao.IProductDao;
 import ssm_service.IProductService;
 
@@ -11,13 +13,14 @@ import java.util.List;
  * @author
  * @since 2021-06-30 16:51
  */
-
+@Service
+@Transactional
 public class ProductServiceImpl implements IProductService {
     @Autowired
     private IProductDao productDao;
 
     @Override
     public List<Product> findAll() throws Exception {
-        return null;
+        return productDao.findAll();
     }
 }
